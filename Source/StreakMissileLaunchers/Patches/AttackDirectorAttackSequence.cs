6becb716dd317873d5fcc34e4528eb6a52b6ce7f
Mad <github@typ03.de>
2020-03-22 22:51:27 +0100

    add targeting laser tests

--- Source/StreakMissileLaunchers/Patches/AttackDirectorAttackSequence.cs
+++ Source/StreakMissileLaunchers/Patches/AttackDirectorAttackSequence.cs
@@ -1,16 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Threading;
 using BattleTech;
 using Harmony;
 using StreakMissileLaunchers.Extensions;
+using UnityEngine;
 
 namespace StreakMissileLaunchers.Patches
 {
     internal static class AttackDirectorAttackSequence
     {
-        internal static bool StreaksScopeEnabled = false;
-        internal static bool StreaksWillHit = false;
+        internal static bool StreakScopeEnabled = false;
+        internal static bool StreakWillHit = false;
+
+
+        [HarmonyPatch(typeof(Mech), "InitGameRep")]
+        public static class Mech_InitStats_Patch
+        {
+            public static void Prefix(Mech __instance)
+            {
+                try
+                {
+                    Logger.Debug($"[Mech_InitGameRep_PREFIX] ({__instance.DisplayName}) Add streak targetting lasers if needed...");
+
+                    bool hasStreaksMounted = false;
+                    foreach(Weapon w in __instance.Weapons)
+                    {
+                        if (w.Type == WeaponType.SRM)
+                        {
+                            hasStreaksMounted = true;
+                        }
+                    }
+                    Logger.Info($"[Mech_InitGameRep_PREFIX] ({__instance.DisplayName}) hasStreaksMounted: {hasStreaksMounted}");
+
+                    if (hasStreaksMounted)
+                    {
+                        AbstractActor actor = __instance as AbstractActor;
+                        WeaponDef weaponDef = actor.Combat.DataManager.WeaponDefs.Get("Weapon_TAG_Standard_0-STOCK");
+                        MechComponentRef mechComponentRef = new MechComponentRef(weaponDef.Description.Id, weaponDef.Description.Id + "_StreakTargettingLaser", ComponentType.Weapon, ChassisLocations.CenterTorso, -1, ComponentDamageLevel.Functional, false);
+                        mechComponentRef.SetComponentDef(weaponDef);
+                        mechComponentRef.DataManager = actor.Combat.DataManager;
+                        mechComponentRef.RefreshComponentDef();
+                        Weapon fakeTargetingLaser = new Weapon(__instance, actor.Combat, mechComponentRef, actor.Combat.GUID + "_StreakTargettingLaser");
+                        fakeTargetingLaser.Init();
+                        fakeTargetingLaser.InitStats();
+
+                        // Add to supportComponents which will get their Representation ready in original method...
+                        __instance.supportComponents.Add(fakeTargetingLaser);
+
+                        Logger.Info($"[Mech_InitGameRep_PREFIX] ({__instance.DisplayName}) fakeTargetingLaser.uid: {fakeTargetingLaser.uid}");
+                        Logger.Info($"[Mech_InitGameRep_PREFIX] ({__instance.DisplayName}) fakeTargetingLaser.weaponDef.WeaponEffectID: {fakeTargetingLaser.weaponDef.WeaponEffectID}");
+
+
+                    }
+
+                    
+
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e);
+                }
+            }
+        }
+
+
 
         [HarmonyPatch(typeof(AttackDirector.AttackSequence), "GetIndividualHits")]
         public static class AttackDirector_AttackSequence_GetIndividualHits_Patch
@@ -33,24 +88,23 @@ namespace StreakMissileLaunchers.Patches
             {
                 try
                 {
-                    //Logger.Info($"[AttackDirector_AttackSequence_GetIndividualHits_PREFIX] Streaks.streaksScopeEnabled: {Streaks.streaksScopeEnabled}");
-                    if (StreaksScopeEnabled && weapon.Type == WeaponType.SRM)
+                    //Logger.Info($"[AttackDirector.AttackSequence_GetIndividualHits_PREFIX] Streaks.streaksScopeEnabled: {Streaks.streaksScopeEnabled}");
+                    if (StreakScopeEnabled && weapon.Type == WeaponType.SRM)
                     {
-                        Logger.Info($"[AttackDirector_AttackSequence_GetIndividualHits_PREFIX] ---");
-                        Logger.Info($"[AttackDirector_AttackSequence_GetIndividualHits_PREFIX] ({weapon.Name}) toHitChance: {toHitChance}");
-                        Logger.Info($"[AttackDirector_AttackSequence_GetIndividualHits_PREFIX] ({weapon.Name}) StreaksScopeEnabled: {StreaksScopeEnabled}");
+                        Logger.Info($"[AttackDirector.AttackSequence_GetIndividualHits_PREFIX] ---");
+                        Logger.Info($"[AttackDirector.AttackSequence_GetIndividualHits_PREFIX] ({weapon.Name}) toHitChance: {toHitChance}");
+                        Logger.Info($"[AttackDirector.AttackSequence_GetIndividualHits_PREFIX] ({weapon.Name}) StreaksScopeEnabled: {StreakScopeEnabled}");
 
-                        if (StreaksWillHit)
+                        if (StreakWillHit)
                         {
                             toHitChance = 1f;
                         }
-
-                        Logger.Info($"[AttackDirector_AttackSequence_GetIndividualHits_PREFIX] ({weapon.Name}) toHitChance: {toHitChance}");
+                        Logger.Info($"[AttackDirector.AttackSequence_GetIndividualHits_PREFIX] ({weapon.Name}) toHitChance: {toHitChance}");
 
                         // Redirecting to hit determination method for clustered hits (LRMs only by default)
                         AttackSequenceGetClusteredHits.Invoke(__instance, new object[] { hitInfo, groupIdx, weaponIdx, weapon, toHitChance, prevDodgedDamage });
 
-                        // Log
+                        Logger.Info($"[AttackDirector.AttackSequence_GetIndividualHits_PREFIX] ({weapon.Name}) Determining clustered hits...");
                         Utilities.LogHitLocations(hitInfo);
 
                         // Hit locations are set, skipping original method...
@@ -71,21 +125,51 @@ namespace StreakMissileLaunchers.Patches
 
 
 
+        // NOTE that this is triggered by OnAttackSequenceFire to iterate through the weapons
+        [HarmonyPatch(typeof(AttackDirector.AttackSequence), "OnAttackSequenceWeaponPreFireComplete")]
+        public static class AttackDirector_AttackSequence_OnAttackSequenceWeaponPreFireComplete_Patch
+        {
+            public static void Postfix(AttackDirector.AttackSequence __instance, MessageCenterMessage message, List<List<Weapon>> ___sortedWeapons, WeaponHitInfo?[][] ___weaponHitInfo)
+            {
+                try
+                {
+                    AttackSequenceWeaponPreFireCompleteMessage attackSequenceWeaponPreFireCompleteMessage = (AttackSequenceWeaponPreFireCompleteMessage)message;
+                    if (attackSequenceWeaponPreFireCompleteMessage.sequenceId != __instance.id)
+                    {
+                        return;
+                    }
+                    int groupIdx = attackSequenceWeaponPreFireCompleteMessage.groupIdx;
+                    int weaponIdx = attackSequenceWeaponPreFireCompleteMessage.weaponIdx;
+                    Weapon weapon = ___sortedWeapons[groupIdx][weaponIdx];
+
+                    Logger.Info($"[AttackDirector.AttackSequence_OnAttackSequenceWeaponPreFireComplete_POSTFIX] ({weapon.parent.DisplayName}) HANDLED AttackSequence: {__instance.id}, WeaponGroup: {groupIdx}, Weapon: {weapon.Name}({weaponIdx})");
+                    Logger.Info($"---");
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e);
+                }
+            }
+        }
+
         [HarmonyPatch(typeof(AttackDirector.AttackSequence), "OnAttackSequenceFire")]
         public static class AttackDirector_AttackSequence_OnAttackSequenceFire_Patch
         {
-            public static void Prefix(AttackDirector.AttackSequence __instance, MessageCenterMessage message, List<List<Weapon>> ___sortedWeapons, ref int[][] ___numberOfShots, ref WeaponHitInfo?[][] ___weaponHitInfo)
+            public static bool Prefix(AttackDirector.AttackSequence __instance, MessageCenterMessage message, List<List<Weapon>> ___sortedWeapons, ref int[][] ___numberOfShots, ref WeaponHitInfo?[][] ___weaponHitInfo)
             {
                 try
                 {
                     AttackSequenceFireMessage attackSequenceFireMessage = (AttackSequenceFireMessage)message;
                     if (attackSequenceFireMessage.sequenceId != __instance.id)
                     {
-                        return;
+                        return false;
                     }
                     int groupIdx = attackSequenceFireMessage.groupIdx;
                     int weaponIdx = attackSequenceFireMessage.weaponIdx;
                     Weapon weapon = ___sortedWeapons[groupIdx][weaponIdx];
+
+                    Logger.Info($"---");
+                    Logger.Info($"[AttackDirector.AttackSequence_OnAttackSequenceFire_PREFIX] ({weapon.parent.DisplayName}) AttackSequence: {__instance.id}, WeaponGroup: {groupIdx}, Weapon: {weapon.Name}({weaponIdx})");
 
                     //if(weapon.weaponDef.ComponentTags.Contains("component_type_streak"))
 
@@ -93,37 +177,77 @@ namespace StreakMissileLaunchers.Patches
                     if (weapon.Type == WeaponType.SRM)
                     {
                         // Enable scope for depending methods and reset hit indicator
-                        StreaksScopeEnabled = true;
-                        StreaksWillHit = false;
-                        Logger.Info($"[AttackDirector_AttackSequence_OnAttackSequenceFire_PREFIX] ---");
-                        Logger.Info($"[AttackDirector_AttackSequence_OnAttackSequenceFire_PREFIX] StreaksScopeEnabled: {StreaksScopeEnabled}");
+                        StreakScopeEnabled = true;
+                        StreakWillHit = false;
+                        Logger.Info($"[AttackDirector.AttackSequence_OnAttackSequenceFire_PREFIX] StreaksScopeEnabled: {StreakScopeEnabled}");
 
                         float hitChance = __instance.Director.Combat.ToHit.GetToHitChance(__instance.attacker, weapon, __instance.chosenTarget, __instance.attackPosition, __instance.chosenTarget.CurrentPosition, __instance.numTargets, __instance.meleeAttackType, __instance.isMoraleAttack);
-                        Logger.Info($"[AttackDirector_AttackSequence_OnAttackSequenceFire_PREFIX] ({weapon.Name}) hitChance: {hitChance}");
+                        Logger.Info($"[AttackDirector.AttackSequence_OnAttackSequenceFire_PREFIX] ({weapon.Name}) hitChance: {hitChance}");
 
                         float hitRoll = UnityEngine.Random.Range(0f, 1f);
-                        Logger.Info($"[AttackDirector_AttackSequence_OnAttackSequenceFire_PREFIX] ({weapon.Name}) hitRoll: {hitRoll}");
+                        Logger.Info($"[AttackDirector.AttackSequence_OnAttackSequenceFire_PREFIX] ({weapon.Name}) hitRoll: {hitRoll}");
 
-                        StreaksWillHit = hitRoll <= hitChance;
-                        Logger.Info($"[AttackDirector_AttackSequence_OnAttackSequenceFire_PREFIX] ({weapon.Name}) StreaksWillHit: {StreaksWillHit}");
+                        StreakWillHit = hitRoll <= hitChance;
+                        Logger.Info($"[AttackDirector.AttackSequence_OnAttackSequenceFire_PREFIX] ({weapon.Name}) StreaksWillHit: {StreakWillHit}");
 
 
 
-                        if (StreaksWillHit)
+                        //
+                        // @ToDo: Try to play some weapon effect to imitate targeting lasers
+                        //
+
+                        //WeaponHitInfo? whiClone = ___weaponHitInfo[groupIdx][weaponIdx];
+                        //Utilities.CreateAndFireStreakTargetingLaser(weapon, whiClone);
+
+                        foreach (Weapon w in weapon.parent.supportComponents)
+                        {
+                            if (w.weaponDef.Description.Id == "Weapon_TAG_Standard_0-STOCK")
+                            {
+                                Logger.Info($"[AttackDirector.AttackSequence_OnAttackSequenceFire_PREFIX] Found fakeTargetingLaser");
+                                //WeaponHitInfo? weaponHitInfo = ___weaponHitInfo[groupIdx][weaponIdx];
+                                //WeaponHitInfo whi;
+                                //if (weaponHitInfo != null)
+                                //{
+                                //    whi = weaponHitInfo.Value;
+                                //    if (w.weaponRep != null && w.weaponRep.HasWeaponEffect)
+                                //    {
+                                //        w.weaponRep.PlayWeaponEffect(whi);
+                                //    }
+                                //}
+
+                                Vector3 fakeTargetingHitPosition = __instance.chosenTarget.GameRep.GetHitPosition(8) + UnityEngine.Random.insideUnitSphere * 5f;
+                                WeaponHitInfo fakeTargetingHitInfo = new WeaponHitInfo(__instance.stackItemUID, __instance.id, 0, 0, __instance.attacker.GUID, __instance.chosenTarget.GUID, 1, new float[1], new float[1], new float[1], new bool[1], new int[] { 8 }, new int[1], new AttackImpactQuality[1], new AttackDirection[1], new Vector3[] { fakeTargetingHitPosition }, new string[1], new int[1]);
+                                Logger.Info($"[AttackDirector.AttackSequence_OnAttackSequenceFire_PREFIX] fakeTargetingHitInfo: {fakeTargetingHitInfo}");
+
+                                if (w.weaponRep != null && w.weaponRep.HasWeaponEffect)
+                                {
+                                    w.weaponRep.PlayWeaponEffect(fakeTargetingHitInfo);
+                                }
+                            }
+                        }
+                        
+
+
+
+                        if (StreakWillHit)
                         {
                             // Force recalculation of HitOnfo by setting it to null here, triggering AttackDirector.AttackSequence.GenerateHitInfo() => AttackDirector.AttackSequence.GetIndividualHits()
                             // There the hit chance of all missiles is set to 1f -> All missiles hit!
                             ___weaponHitInfo[groupIdx][weaponIdx] = null;
+
+                            return true;
                         }
                         else
                         {
                             // This should cancel firing, see code of original method...
-                            ___numberOfShots[groupIdx][weaponIdx] = -1;
-                            Logger.Info($"[AttackDirector_AttackSequence_OnAttackSequenceFire_PREFIX] ({weapon.Name}) Lock on failed, setting ___numberOfShots to -1");
+                            //___numberOfShots[groupIdx][weaponIdx] = -1;
+
+                            Logger.Info($"[AttackDirector.AttackSequence_OnAttackSequenceFire_PREFIX] ({weapon.Name}) Lock on failed, setting ___numberOfShots to -1");
 
                             // Mark Streak SRMs as having fired nevertheless because a failed lock on should be handled like "fired"
                             new Traverse(weapon).Property("HasFired").SetValue(true);
-                            Logger.Info($"[AttackDirector_AttackSequence_OnAttackSequenceFire_PREFIX] ({weapon.Name}) HasFired: {weapon.HasFired}");
+                            weapon.CompleteFiring();
+                            Logger.Info($"[AttackDirector.AttackSequence_OnAttackSequenceFire_PREFIX] ({weapon.Name}) HasFired: {weapon.HasFired}, RoundsSinceLastFire: {weapon.roundsSinceLastFire}");
 
                             // Floaties
                             __instance.Director.Combat.MessageCenter.PublishMessage(new FloatieMessage(weapon.parent.GUID, weapon.parent.GUID, "STREAK LOCK-ON FAILED", FloatieMessage.MessageNature.Debuff));
@@ -135,19 +259,59 @@ namespace StreakMissileLaunchers.Patches
                             {
                                 weapon.OffsetAmmo();
                             }
+
+                            // Cancel firing, send messages to signal completion of handling this weapon
+                            // BEWARE: If these messages are sent too early the order of handled weapons will be disturbed with chaotic consequences for the sequence!!!
+                            //AttackSequenceWeaponPreFireCompleteMessage messageWeaponPreFireComplete = new AttackSequenceWeaponPreFireCompleteMessage(__instance.stackItemUID, __instance.id, groupIdx, weaponIdx);
+                            //__instance.Director.Combat.MessageCenter.PublishMessage(messageWeaponPreFireComplete);
+                            //AttackSequenceWeaponCompleteMessage messageWeaponComplete = new AttackSequenceWeaponCompleteMessage(__instance.stackItemUID, __instance.id, groupIdx, weaponIdx);
+                            //__instance.Director.Combat.MessageCenter.PublishMessage(messageWeaponComplete);
+
+                            return false;
                         }
+                    }
+                    else
+                    {
+                        return true;
                     }
                 }
                 catch (Exception e)
                 {
                     Logger.Error(e);
+                    return true;
                 }
             }
 
-            public static void Postfix(AttackDirector.AttackSequence __instance)
+            public static void Postfix(AttackDirector.AttackSequence __instance, MessageCenterMessage message, List<List<Weapon>> ___sortedWeapons, ref int[][] ___numberOfShots, ref WeaponHitInfo?[][] ___weaponHitInfo)
             {
-                StreaksScopeEnabled = false;
-                Logger.Info($"[AttackDirector_AttackSequence_OnAttackSequenceFire_POSTFIX] StreaksScopeEnabled: {StreaksScopeEnabled}");
+                /*
+                AttackSequenceFireMessage attackSequenceFireMessage = (AttackSequenceFireMessage)message;
+                if (attackSequenceFireMessage.sequenceId != __instance.id)
+                {
+                    return;
+                }
+                int groupIdx = attackSequenceFireMessage.groupIdx;
+                int weaponIdx = attackSequenceFireMessage.weaponIdx;
+                */
+
+                if (StreakScopeEnabled)
+                {
+                    /*
+                    if (!StreakWillHit)
+                    {
+                        // Sent messages for the canceled shots to prevent stalling the whole sequence
+                        AttackSequenceWeaponPreFireCompleteMessage messageWeaponPreFireComplete = new AttackSequenceWeaponPreFireCompleteMessage(__instance.stackItemUID, __instance.id, groupIdx, weaponIdx);
+                        __instance.Director.Combat.MessageCenter.PublishMessage(messageWeaponPreFireComplete);
+                        AttackSequenceWeaponCompleteMessage messageWeaponComplete = new AttackSequenceWeaponCompleteMessage(__instance.stackItemUID, __instance.id, groupIdx, weaponIdx);
+                        __instance.Director.Combat.MessageCenter.PublishMessage(messageWeaponComplete);
+                    }
+                    */
+
+
+                    StreakScopeEnabled = false;
+                    Logger.Info($"[AttackDirector.AttackSequence_OnAttackSequenceFire_POSTFIX] StreaksScopeEnabled: {StreakScopeEnabled}");
+                    Logger.Info($"---");
+                }
             }
         }
     }
--- Source/StreakMissileLaunchers/Utilities.cs
+++ Source/StreakMissileLaunchers/Utilities.cs
@@ -1,32 +1,120 @@
 ﻿using System;
+using System.Collections.Generic;
 using BattleTech;
+using UnityEngine;
 
 namespace StreakMissileLaunchers
 {
     class Utilities
     {
+        public static void CreateAndFireStreakTargetingLaser(Weapon baseWeapon, WeaponHitInfo? baseWeaponHitInfo)
+        {
+            try
+            {
+                AbstractActor actor = baseWeapon.parent;
+                if (!(actor is Mech mech))
+                {
+                    return;
+                }
+
+                // Borrowed from AbstractActor.InitAbilities()
+                WeaponDef weaponDef = actor.Combat.DataManager.WeaponDefs.Get("Weapon_Laser_SmallLaserPulse_0-STOCK");
+                MechComponentRef mechComponentRef = new MechComponentRef(weaponDef.Description.Id, weaponDef.Description.Id + "_StreakTargettingLaser", ComponentType.Weapon, (ChassisLocations)baseWeapon.Location, -1, baseWeapon.DamageLevel, false);
+                mechComponentRef.SetComponentDef(weaponDef);
+                mechComponentRef.DataManager = actor.Combat.DataManager;
+                mechComponentRef.RefreshComponentDef();
+                Weapon fakeTargetingLaser = new Weapon(mech, actor.Combat, mechComponentRef, actor.Combat.GUID + "_StreakTargettingLaser");
+                fakeTargetingLaser.Init();
+                fakeTargetingLaser.InitStats();
+                mech.supportComponents.Add(fakeTargetingLaser);
+                Logger.Info($"[TEST] fakeTargetingLaser.weaponDef.WeaponEffectID: {fakeTargetingLaser.weaponDef.WeaponEffectID}");
+
+
+                // Borrowed from Mech.InitGameRep()
+                List<string> usedPrefabNames = new List<string>();
+                fakeTargetingLaser.baseComponentRef.prefabName = MechHardpointRules.GetComponentPrefabName(mech.MechDef.Chassis.HardpointDataDef, fakeTargetingLaser.baseComponentRef, mech.MechDef.Chassis.PrefabBase, fakeTargetingLaser.mechComponentRef.MountedLocation.ToString().ToLower(), ref usedPrefabNames);
+                fakeTargetingLaser.baseComponentRef.hasPrefabName = true;
+                if (!string.IsNullOrEmpty(fakeTargetingLaser.baseComponentRef.prefabName))
+                {
+                    Transform attachTransform = mech.GetAttachTransform(fakeTargetingLaser.mechComponentRef.MountedLocation);
+                    fakeTargetingLaser.InitGameRep(fakeTargetingLaser.baseComponentRef.prefabName, attachTransform, actor.LogDisplayName);
+
+                    mech.GameRep.weaponReps.Add(fakeTargetingLaser.weaponRep);
+                    string componentMountingPointPrefabName = MechHardpointRules.GetComponentMountingPointPrefabName(mech.MechDef, fakeTargetingLaser.mechComponentRef);
+                    if (!string.IsNullOrEmpty(componentMountingPointPrefabName))
+                    {
+                        WeaponRepresentation component = actor.Combat.DataManager.PooledInstantiate(componentMountingPointPrefabName, BattleTechResourceType.Prefab, null, null, null).GetComponent<WeaponRepresentation>();
+                        component.Init(mech, attachTransform, true, actor.LogDisplayName, fakeTargetingLaser.Location);
+                        mech.GameRep.weaponReps.Add(component);
+                    }
+                }
+
+
+                // Borrowed from AttackDirector.AttackSequence.OnAttackSequenceFire()
+                WeaponHitInfo? weaponHitInfo = baseWeaponHitInfo;
+                Logger.Info($"[TEST] weaponHitInfo: {weaponHitInfo}");
+                if (weaponHitInfo != null)
+                {
+                    WeaponHitInfo whi;
+                    whi = weaponHitInfo.Value;
+                    Logger.Info($"[TEST] fakeTargetingLaser.weaponRep: {fakeTargetingLaser.weaponRep}");
+                    Logger.Info($"[TEST] fakeTargetingLaser.weaponRep.HasWeaponEffect: {fakeTargetingLaser.weaponRep.HasWeaponEffect}");
+                    if (fakeTargetingLaser.weaponRep != null && fakeTargetingLaser.weaponRep.HasWeaponEffect)
+                    {
+                        // ToDo: Disable sound for this effect
+                        fakeTargetingLaser.weaponRep.PlayWeaponEffect(whi);
+                    }
+                }
+
+
+                // Cleanup
+                /*
+                foreach (Weapon w in mech.Weapons)
+                {
+                    Logger.Info($"[TEST] mech.Weapons: {w.Name}({w.uid})");
+                }
+                foreach (MechComponent c in mech.supportComponents)
+                {
+                    Logger.Info($"[TEST] mech.supportComponents: {c.Name}({c.GUID})");
+                }
+                */
+
+                mech.GameRep.weaponReps.Remove(fakeTargetingLaser.weaponRep);
+                mech.supportComponents.Remove(fakeTargetingLaser);
+                fakeTargetingLaser = null;
+
+
+
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e);
+            }
+        }
+
         public static void LogHitLocations(WeaponHitInfo hitInfo)
         {
             try
             {
-                string output = "[Utilities_LogHitLocations] ---\n";
+                string output = "---\n";
                 output += $"[Utilities_LogHitLocations] Clustered hits: {hitInfo.hitLocations.Length}\n";
                 for (int i = 0; i < hitInfo.hitLocations.Length; i++)
                 {
                     int location = hitInfo.hitLocations[i];
-                    var chassisLocationFromArmorLocation =
-                        MechStructureRules.GetChassisLocationFromArmorLocation((ArmorLocation)location);
+                    var chassisLocationFromArmorLocation = MechStructureRules.GetChassisLocationFromArmorLocation((ArmorLocation)location);
 
                     if (location == 0 || location == 65536)
                     {
-                        output += $"[Utilities_LogHitLocations] HitLocation {i}: NONE/INVALID\n";
+                        output += $"[Utilities_LogHitLocations] HitLocation {i}: NONE/INVALID";
                     }
                     else
                     {
-                        output += $"[Utilities_LogHitLocations] HitLocation {i}: {chassisLocationFromArmorLocation} ({location})\n";
+                        output += $"[Utilities_LogHitLocations] HitLocation {i}: {chassisLocationFromArmorLocation}({location})";
                     }
+
+                    output += (i < hitInfo.hitLocations.Length -1) ? "\n" : "\n---";
                 }
-                Logger.Info(output);
+                Logger.Info(output, false);
             }
             catch (Exception e)
             {
